1c352b9c972dd17ee4d3f76dcc6b1b7b2a1614c9
M00031 <mchand18@outlook.com>
2016-10-26 14:33:00 -0700

    Zoom changes... in progress

--- src/main/java/com/sarahhand/fractals/view/FractalsUI.java
+++ src/main/java/com/sarahhand/fractals/view/FractalsUI.java
@@ -4,22 +4,31 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.geom.Point2D.Double;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+import com.sarahhand.fractals.model.ColorPalette;
 import com.sarahhand.fractals.model.MandelbrotConfig;
 import com.sarahhand.fractals.model.MandelbrotViewer;
 import com.sarahhand.fractals.model.MandelbrotViewerFactory;
 
+/** This class will display the Mandelbrot set and allow you to do a variety of other things.
+ * 
+ * @author mhand
+ *
+ */
 public class FractalsUI implements MouseListener {
 	
-	JFrame frame;
-	Dimension frameDimension;
+	private JFrame frame;
+	private Dimension frameDimension;
 	MandelbrotViewer viewer;
-	ImageIcon image;
-	JLabel imageLabel;
+	private MandelbrotConfig mandelConfig;
+	private ColorPalette palette;
+	private ImageIcon image;
+	private JLabel imageLabel;
 	
 	public FractalsUI() {
 		frame = new JFrame("Fractals Drawer");
@@ -28,6 +37,7 @@ public class FractalsUI implements MouseListener {
 		frame.setSize(frameDimension);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		viewer = MandelbrotViewerFactory.createViewer();
+		mandelConfig = MandelbrotConfig.DEAFAULT_CONFIG;
 		image = new ImageIcon(viewer.getView(frameDimension));
 		imageLabel = new JLabel(image);
 		frame.add(imageLabel);
@@ -40,11 +50,14 @@ public class FractalsUI implements MouseListener {
 		int buttonType = me.getButton();
 		int mouseX = me.getX();
 		int mouseY = me.getY();
+		Double center = new Double(mouseX, mouseY);
+		mandelConfig.setCenter(center);
 		if(buttonType == 1) {
-			
+			mandelConfig.setZoom(mandelConfig.getZoom() * 10);
 		} else if(buttonType == 3) {
-			
+			mandelConfig.setZoom(mandelConfig.getZoom() / 10);
 		}
+		viewer.getView(frameDimension);
 	}
 
 	public void mouseEntered(MouseEvent me) {}

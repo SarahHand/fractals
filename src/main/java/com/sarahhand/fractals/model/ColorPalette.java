7ee39d82fbfe5ac3546902f8e53364319eb4215a
J9465812 <jehand997@outlook.com>
2016-10-28 13:09:11 -0700

    Cleaned up code. Added cardiod/bulb checking optimizations.

--- src/main/java/com/sarahhand/fractals/model/ColorPalette.java
+++ src/main/java/com/sarahhand/fractals/model/ColorPalette.java
@@ -5,13 +5,14 @@ import java.awt.Color;
 public class ColorPalette{
 
 	static final ColorPalette DEFAULT_PALETTE;
+	public static final int COLOR_PALETTE_LENGTH = 200;
 
 	static{
 
-		Color[] palette = new Color[200];
+		Color[] palette = new Color[COLOR_PALETTE_LENGTH];
 
-		for (int n = 0; n < 200; n++) {
-			Color color = Color.getHSBColor((float) n / 200, 1f, 1f);
+		for (int n = 0; n < COLOR_PALETTE_LENGTH; n++) {
+			Color color = Color.getHSBColor((float) n / COLOR_PALETTE_LENGTH, 1f, 1f);
 			palette[n] = color;
 		}
 

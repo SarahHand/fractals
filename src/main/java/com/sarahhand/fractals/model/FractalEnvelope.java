650cf2c7a2b034764458d0a0f9bd1c7c3134868b
M00031 <mchand18@outlook.com>
2017-01-11 13:17:38 -0800

    The new FractalEnvelope class

---
+++ src/main/java/com/sarahhand/fractals/model/FractalEnvelope.java
@@ -0,0 +1,14 @@
+package com.sarahhand.fractals.model;
+
+public class FractalEnvelope {
+
+	private FractalConfig config;
+
+	public FractalConfig getConfig() {
+		return config;
+	}
+
+	public void setConfig(FractalConfig config) {
+		this.config = config;	
+	}
+}
